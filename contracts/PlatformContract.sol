7270312fa2f4b1190d5738ff04546b21d2e9a0e7
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-01-14 14:58:19 +0100

    Plans in platform contract

---
+++ contracts/PlatformContract.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract PlatformContract {
+    using Counters for Counters.Counter;
+    Counters.Counter private _planIds;
+
+    struct Plan {
+        uint8 id;
+        uint256 price;
+        uint256 duration;
+    }
+    Plan[] private plans;
+
+    function addPlan(uint256 _price, uint256 _duration) public {
+        Plan memory p = Plan({
+            id: uint8(_planIds.current()),
+            price: _price,
+            duration: _duration
+        });
+        _planIds.increment();
+        plans.push(p);
+    }
+
+    function removePlan(uint8 _id) public {
+        for (uint8 i = 0; i < plans.length; i++) {
+            if (plans[i].id == _id) {
+                plans[i] = plans[plans.length - 1];
+                plans.pop();
+                break;
+            }
+        }
+    }
+
+    function getPlan(uint8 _id) public view returns (Plan memory) {
+        return plans[_id];
+    }
+
+    function getPlans() public view returns (Plan[] memory) {
+        return plans;
+    }
+}
