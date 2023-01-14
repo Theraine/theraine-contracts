f6440bffc7de3e74a0ade8a6195d7d17006b703a
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-01-14 15:00:06 +0100

    Create new platform in Parent contract

---
+++ contracts/ParentContract.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./PlatformContract.sol";
+
+contract ParentContract {
+    using Counters for Counters.Counter;
+    Counters.Counter private _platformIds;
+    struct Platform {
+        uint256 id;
+        bytes32 details;
+    }
+
+    Platform[] private platforms;
+
+    /**
+     * @dev Creates a new platform.
+     */
+    function createPlatform(bytes32 _details) public returns(address) {
+        Platform memory p = Platform({
+            id: _platformIds.current(),
+            details: _details
+        });
+        platforms.push(p);
+        PlatformContract pc = new PlatformContract();
+        _platformIds.increment();
+        return address(pc);
+    }
+}
