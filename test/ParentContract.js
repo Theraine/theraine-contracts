ca3864051e28907de83a14868feecdef152ecc5f
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-01-15 03:19:14 +0100

    Add test for parent contract

---
+++ test/ParentContract.js
@@ -0,0 +1,37 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const web3 = require("web3");
+const PlatformContract = require("../artifacts/contracts/PlatformContract.sol/PlatformContract.json");
+
+describe("Parent Contract Tests", function() {
+    let parent;
+    let platform;
+    let platformAddress;
+
+    beforeEach(async function() {
+        const Parent = await ethers.getContractFactory("ParentContract");
+        parent = await Parent.deploy();
+    });
+
+    describe("Parent Contract", function() {
+        it("creates a new platform", async function() {
+            const details = web3.utils.fromAscii("Test Platform");
+            await parent.createPlatform(details);
+            const platformStruct = await parent.getPlatform(0);
+            
+            expect(platformStruct.details).to.equal(details);
+        });
+
+        it("gets all platforms", async function() {
+            const details1 = web3.utils.fromAscii("Test Platform 1");
+            const details2 = web3.utils.fromAscii("Test Platform 2");
+            await parent.createPlatform(details1);
+            await parent.createPlatform(details2);
+            const platforms = await parent.getPlatforms();
+
+            expect(platforms.length).to.equal(2);
+            expect(platforms[0].details).to.equal(details1);
+            expect(platforms[1].details).to.equal(details2);
+        });
+    });
+});    
